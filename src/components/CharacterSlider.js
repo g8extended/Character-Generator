6f91756b22ca5af29b70bb0efa5590370ee8d7c0
Alexei Online <alexei.online.work@gmail.com>
2017-02-21 17:50:34 +0300

    update some styles

--- src/components/CharacterSlider.js
+++ src/components/CharacterSlider.js
@@ -11,17 +11,13 @@ export default class CharacterSlider extends Component {
         <li className="character">
           <img src="svg/Hairstyles/hairstyle_02.svg" />
         </li>
-        <li>
-          left arrow
-        </li>
+        <li className="arrow"> &#x276E; </li>
         <li className="character">
           <img src="svg/Body/body.svg" />
           <img src="svg/Hairstyles/hairstyle_03.svg" />
           <img src="svg/Shirts/shirt_03.svg" />
         </li>
-        <li>
-          right arrow
-        </li>
+        <li className="arrow"> &#x276F; </li>
         <li className="character">
           <img src="svg/Hairstyles/hairstyle_04.svg" />
         </li>
