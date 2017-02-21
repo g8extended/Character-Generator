5e484e938f30eebc75255c3d2a9966f8e33c291a
Eugene Glova <eugene.glova@gmail.com>
2017-02-21 14:20:05 +0300

    Add menu

--- src/App.js
+++ src/App.js
@@ -3,9 +3,26 @@ import CharacterSlider from './components/CharacterSlider';
 import styles from './styles/app.scss';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: 'Hairstyles'
+    };
+  }
   render() {
     return (
-      <CharacterSlider />
+      <div>
+        <ul>
+          {['Hairstyles', 'Eyes', 'Mouth', 'Beards', 'Clothes', 'Accessories'].map((item, index) => {
+            if (this.state.active === item) {
+              return (<li key={index} className="active">{item}</li>);
+            } else {
+              return (<li key={index} onClick={() => this.setState({ active: item })}>{item}</li>);
+            }
+          })}
+        </ul>
+        <CharacterSlider />
+      </div>
     );
   }
 }
