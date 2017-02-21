acea3ff02d4a2800a14b633763ccc741328f5034
Eugene Glova <eugene.glova@gmail.com>
2017-02-21 22:26:38 +0300

    Update header nav

--- src/reducers/headerNav.js
+++ src/reducers/headerNav.js
@@ -1,10 +1,39 @@
 import {
   HEADER_NAV_CLICK
 } from '../constants/headerNav';
+import keyBy from 'lodash/keyBy';
 
 const initialState = {
-	items: ['Hairstyles', 'Eyes', 'Mouth', 'Beards', 'Clothes', 'Accessories'],
-	selected: 0
+  items: keyBy([
+    {
+      id: 0,
+      title: 'Hairstyles',
+      assetID: 'Hairstyles'
+    },
+    {
+      id: 1,
+      title: 'Eyes',
+      assetID: 'Eyes'
+    },
+    {
+      id: 2,
+      title: 'Mouth'
+    },
+    {
+      id: 3,
+      title: 'Beards',
+      assetID: 'Beards'
+    },
+    {
+      id: 4,
+      title: 'Clothes'
+    },
+    {
+      id: 5,
+      title: 'Accessories'
+    }
+  ], 'id'),
+  selected: 0
 };
 
 const reducer = (state = initialState, action) => {
