f48ac2cef442fd9c9d86362ce3976203642ee7e4
Eugene Glova <eugene.glova@gmail.com>
2017-02-21 20:36:25 +0300

    Add server side api to return structure of svg files

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var webpack = require('webpack');
 var express = require('express');
+var fs = require('fs');
 var config = require('./webpack.config');
 
 var app = express();
@@ -12,6 +13,19 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.use(require('webpack-hot-middleware')(compiler));
 app.use(express.static('public'));
+
+app.get('/api/assets/', function (req, res) {
+  const svgPath = 'public/svg/';
+  const svgFolders = fs.readdirSync(svgPath).filter(name => (
+    fs.statSync(path.join(svgPath, name)).isDirectory()
+  ));
+  const data = svgFolders.map(name => ({
+    id: name,
+    files: fs.readdirSync(path.join(svgPath, name))
+  }));
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(data));
+});
 
 app.listen(3000, function(err) {
   if (err) {
