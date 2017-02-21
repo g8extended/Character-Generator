69500bae2252fe184ddb6cf48f8b43c359f8221a
Eugene Glova <eugene.glova@gmail.com>
2017-02-21 13:56:59 +0300

    Move slider into its own component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
 import React, { Component } from 'react';
-import Body from './svg/Body/body.svg';
-import Hairstyles from './svg/Hairstyles/hairstyle_01.svg';
-import Shirts from './svg/Shirts/shirt_01.svg';
-import styles from './styles/main.scss';
+import CharacterSlider from './components/CharacterSlider';
+import styles from './styles/app.scss';
 
 export default class App extends Component {
   render() {
     return (
-      <ul className="character-slider">
-        <li className="character">
-          <Hairstyles className="hairstyles" />
-        </li>
-        <li className="character">
-          <Hairstyles className="hairstyles" />
-        </li>
-        <li>
-          left arrow
-        </li>
-        <li className="character">
-          <Body className="body" />
-          <Hairstyles className="hairstyles" />
-          <Shirts className="shirts" />
-        </li>
-        <li>
-          right arrow
-        </li>
-        <li className="character">
-          <Hairstyles className="hairstyles" />
-        </li>
-        <li className="character">
-          <Hairstyles className="hairstyles" />
-        </li>
-      </ul>
+      <CharacterSlider />
     );
   }
 }
---
+++ src/components/CharacterSlider.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Body from '../svg/Body/body.svg';
+import Hairstyles from '../svg/Hairstyles/hairstyle_01.svg';
+import Shirts from '../svg/Shirts/shirt_01.svg';
+import styles from '../styles/character-slider.scss';
+
+export default class CharacterSlider extends Component {
+  render() {
+    return (
+      <ul className="character-slider">
+        <li className="character">
+          <Hairstyles className="hairstyles" />
+        </li>
+        <li className="character">
+          <Hairstyles className="hairstyles" />
+        </li>
+        <li>
+          left arrow
+        </li>
+        <li className="character">
+          <Body className="body" />
+          <Hairstyles className="hairstyles" />
+          <Shirts className="shirts" />
+        </li>
+        <li>
+          right arrow
+        </li>
+        <li className="character">
+          <Hairstyles className="hairstyles" />
+        </li>
+        <li className="character">
+          <Hairstyles className="hairstyles" />
+        </li>
+      </ul>
+    );
+  }
+}
