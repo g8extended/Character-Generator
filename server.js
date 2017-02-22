6539403d096a7bcb4be77a8251133fb0fb547bb6
Eugene Glova <eugene.glova@gmail.com>
2017-02-22 22:39:44 +0300

    Read sub folders for colors

--- server.js
+++ server.js
@@ -19,36 +19,97 @@ app.get('/api/assets/', function (req, res) {
   const svgFolders = [
     {
       id: 'Beards',
+      colors: [
+        {
+          id: 'blond',
+        },
+        {
+          id: 'brown-haired person',
+        },
+        {
+          id: 'brunet',
+        },
+        {
+          id: 'white'
+        }
+      ],
       sortOrder: 4
     },
     {
       id: 'Body',
+      colors: [
+        {
+          id: 'default',
+        }
+      ],
       sortOrder: 0
     },
     {
       id: 'Glasses',
+      colors: [
+        {
+          id: 'default',
+        }
+      ],
       sortOrder: 1
     },
     {
       id: 'Hairstyles',
+      colors: [
+        {
+          id: 'blond',
+        },
+        {
+          id: 'brown-haired person',
+        },
+        {
+          id: 'brunet',
+        },
+        {
+          id: 'white'
+        }
+      ],
       sortOrder: 1
     },
     {
       id: 'Scarfes',
+      colors: [
+        {
+          id: 'default',
+        }
+      ],
       sortOrder: 3
     },
     {
       id: 'Shirts',
+      colors: [
+        {
+          id: 'default',
+        }
+      ],
       sortOrder: 1
     },
     {
       id: 'Tie',
+      colors: [
+        {
+          id: 'default',
+        }
+      ],
       sortOrder: 2
     }
   ];
-  const data = svgFolders.map(folder => Object.assign({}, folder,
-    { files: fs.readdirSync(path.join(svgPath, folder.id)) }
-  ));
+  const data = svgFolders.map(folder => {
+    const colors = folder.colors.map(color => {
+      return Object.assign({}, color, {
+        files: fs.readdirSync(path.join(svgPath, folder.id, color.id))
+      });
+    });
+
+    return Object.assign({}, folder, {
+      colors
+    });
+  });
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify(data));
 });
