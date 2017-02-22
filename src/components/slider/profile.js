40d401fb2b21fdf6c58d7f269fc03cc374707680
Eugene Glova <eugene.glova@gmail.com>
2017-02-22 23:43:55 +0300

    Store color and file index for profile

--- src/components/slider/profile.js
+++ src/components/slider/profile.js
@@ -4,19 +4,27 @@ import { assetClick } from '../../actions/assets';
 import map from 'lodash/map';
 import ColorPicker from './colorpicker';
 
+const getImage = (asset, profile, assets) => {
+  const color = profile[asset.id].color;
+  const fileIndex = profile[asset.id].fileIndex;
+  const fileName = assets.data[asset.id].colors[color].files[fileIndex];
+  return `svg/${asset.id}/${color}/${fileName}`;
+};
+
 class Profile extends Component {
   render() {
     const { dispatch, profile, assets } = this.props;
     if ( ! assets.data.Hairstyles) return <div />;
     const data = map(assets.data);
     data.sort((a, b) => a.sortOrder - b.sortOrder);
+
     return (
       <div className="profile"> 
         <div className="character">
           {map(data, asset => (
             <img
               key={asset.id}
-              src={`svg/${asset.id}/${assets.currentColor}/${assets.data[asset.id].colors[assets.currentColor].files[profile[asset.id]]}`}
+              src={getImage(asset, profile, assets)}
               onClick={() => dispatch(assetClick(asset.id))}
             />
           ))}
--- src/reducers/profile.js
+++ src/reducers/profile.js
@@ -9,7 +9,13 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
   case FETCH_ASSETS_FULFILLED:
-    const data = action.payload.data.reduce((memo, asset) => ({ ...memo, [asset.id]: 0 }), {});
+    const data = action.payload.data.reduce((memo, asset) => ({
+      ...memo,
+      [asset.id]: {
+        color: 'default',
+        fileIndex: 0
+      }
+    }), {});
     return { ...state, ...data };
   case SET_ASSET_INDEX:
     return { ...state, [action.key]: action.payload };
