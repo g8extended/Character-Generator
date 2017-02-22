e48a338cc621a9bc6b6102cfb072365aced22839
Eugene Glova <eugene.glova@gmail.com>
2017-02-22 11:23:38 +0300

    Add mapping for assets and header nav and use it when user click navigation to apply selected asset

--- src/actions/headerNav.js
+++ src/actions/headerNav.js
@@ -1,10 +1,19 @@
 import {
   HEADER_NAV_CLICK
 } from '../constants/headerNav';
+import {
+  SET_CURRENT_ASSET
+} from '../constants/assets';
+import { setCurrentAsset } from '../actions/assets';
+import keyBy from 'lodash/keyBy';
 
-export const headerNavClick = index => dispatch => {
+export const headerNavClick = headerNavID => (dispatch, getState) => {
+  dispatch({
+    type: SET_CURRENT_ASSET,
+    payload: keyBy(getState().assetsToHeaderNavMapping, 'headerNavID')[headerNavID].assetID
+  });
   dispatch({
     type: HEADER_NAV_CLICK,
-    payload: index
+    payload: headerNavID
   });
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import headerNav from './headerNav';
 import assets from './assets';
+import assetsToHeaderNavMapping from './assetsToHeaderNavMapping';
 import profile from './profile';
 
 const rootReducer = combineReducers({
   headerNav,
   assets,
+  assetsToHeaderNavMapping,
   profile
 });
 
