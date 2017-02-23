73269cf8205ecc06d2c67bf5ac1e30d10e96ca73
Alexei Online <alexei.online.work@gmail.com>
2017-02-23 16:15:59 +0300

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,12 +4,10 @@ import { buyCurrentProfile } from '../actions/sale';
 export default (
   () => (
     <div className="footer">
-    	<div className="content-wrapper">
-    		<button className="button" onClick={buyCurrentProfile}>buy for 10$</button>	
-	    	<p className="text">
-	    		You'll pay for one character (svg, png)	
-	    	</p>
-	    </div>
+      <div className="content-wrapper">
+        <button className="button" onClick={buyCurrentProfile} >buy for 10$</button>	
+        <p className="text">You buy only one character (svg, png)	</p>
+      </div>
     </div>
   )
 );
