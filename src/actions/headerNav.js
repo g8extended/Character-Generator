b2f2591150c2bcc640fd706eef8cd2ae1cc740ad
Eugene Glova <eugene.glova@gmail.com>
2017-02-23 21:31:12 +0300

    Hide submenu on click sub menu item

--- src/actions/headerNav.js
+++ src/actions/headerNav.js
@@ -30,4 +30,5 @@ export const headerNavClick = headerNavID => (dispatch, getState) => {
   const mapping = keyBy(getState().assetsToHeaderNavMapping, 'headerNavID')[headerNavID];
   mapping && dispatch(setCurrentAsset(mapping.assetID));
   dispatch(setCurrentHeaderNav(headerNavID));
+  dispatch(headevNavMouseLeave());
 };
