0481fe44147fdbb831e5db1ebab6e1f33a55543b
Eugene Glova <eugene.glova@gmail.com>
2017-02-25 13:18:20 +0300

    Add local storage persistence

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ import { render } from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import { Router, Route, browserHistory } from 'react-router';
-import { syncHistoryWithStore, routerMiddleware } from 'react-router-redux'
+import { syncHistoryWithStore, routerMiddleware } from 'react-router-redux';
+import { persistStore, autoRehydrate } from 'redux-persist';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 import App from './App';
 import { fetchAssets } from './actions/assets';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk, routerMiddleware(browserHistory))));
+const store = createStore(reducers, composeEnhancers(
+    applyMiddleware(thunk, routerMiddleware(browserHistory)),
+    autoRehydrate()
+  )
+);
+
+persistStore(store);
 
 const history = syncHistoryWithStore(browserHistory, store);
 
