cdf0ba7e1476114ee2bfa04f368eccfda1d43b82
Eugene Glova <eugene.glova@gmail.com>
2017-02-25 13:03:32 +0300

    Push asset/color url

--- src/actions/assets.js
+++ src/actions/assets.js
@@ -19,26 +19,37 @@ export const fetchAssets = () => dispatch => {
     dispatch({
       type: FETCH_ASSETS_FULFILLED,
       payload: res
-    });    
+    });
+    dispatch(setCurrentAsset(res.data[0].id));
+    dispatch(setCurrentColor(res.data[0].colors[0].id));
   });
 };
 
 export const setCurrentAsset = assetID => (dispatch, getState) => {
-  dispatch(push(`/${assetID.toLowerCase()}`));
+  const state = getState();
+  const profileAsset = state.profile[assetID];
+  const color = profileAsset ? profileAsset.color : state.assets[assetID].colors[0];
+  dispatch(push(`/${assetID.toLowerCase()}/${color}`));
   dispatch({
     type: SET_CURRENT_ASSET,
     payload: {
       assetID,
-      color: getState().profile[assetID].color
+      color: color
     }
   });
 };
 
-export const colorClick = color => dispatch => {
+export const setCurrentColor = color => (dispatch, getState) => {
+  const assetID = getState().assets.current;
+  dispatch(push(`/${assetID.toLowerCase()}/${color}`));
   dispatch({
     type: SET_CURRENT_COLOR,
     payload: color
   });
+};
+
+export const colorClick = color => dispatch => {
+  dispatch(setCurrentColor(color));
   dispatch(updateProfileAssetColor(color));
 };
 
