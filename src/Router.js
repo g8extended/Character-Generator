20f0f13e78bef54a9e6c0583b2dd6d662c1b5ac0
Eugene Glova <eugene.glova@gmail.com>
2017-02-27 23:19:01 +0300

    Add redirect

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Router, Route } from 'react-router';
+import { Router, Route, IndexRedirect } from 'react-router';
 import App from './App';
 import { setCurrentAsset, setCurrentColor } from './actions/assets';
 
@@ -8,7 +8,9 @@ export default connect()(
   ({ dispatch, history }) => (
     <Router history={history}>
       <Route path="/" component={App}>
+        <IndexRedirect to="/assets" />
         <Route path="assets" component={App}>
+          <IndexRedirect to="/assets/Hairstyles" />
           <Route path=":assetID" component={App} onEnter={route => dispatch(setCurrentAsset(route.params.assetID))}>
             <Route path=":color" component={App} onEnter={route => dispatch(setCurrentColor(route.params.color))}>
             </Route>
