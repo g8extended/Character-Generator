2b1daab97b4d5fdf07ff26b7a7c2b8ae80e25a18
Eugene Glova <eugene.glova@gmail.com>
2017-03-03 13:20:42 +0300

    Add positions and dimensions for assets

--- data/assets.js
+++ data/assets.js
@@ -1,13 +1,23 @@
+const { height, width } = { width: 739.6, height: 909.9 };
+
 module.exports = [
   {
     id: 'Hairstyles',
     required: false,
-    sortOrder: 1
+    sortOrder: 1,
+    width: 358 / width * 100 + '%',
+    height: 342.5 / height * 100 + '%',
+    left: 195 / width * 100 + '%',
+    top: 45 / height * 100 + '%'
   },
   {
     id: 'Beards',
     required: false,
-    sortOrder: 5
+    sortOrder: 5,
+    width: 323/ width * 100 + '%',
+    height: 353 / height * 100 + '%',
+    left: 209/ width * 100 + '%',
+    top: 313 / height * 100 + '%'
   },
   {
     id: 'Body',
@@ -45,7 +55,11 @@ module.exports = [
     id: 'Eyes',
     required: true,
     sortOrder: 1,
-    subColors: true
+    subColors: true,
+    width: 172 / width * 100 + '%',
+    height: 77 / height * 100 + '%',
+    left: 287 / width * 100 + '%',
+    top: 275 / height * 100 + '%'
   },
   {
     id: 'Jackets',
