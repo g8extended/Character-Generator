c3e5f91286e75bcf9e51859c976b22030c5e89b5
Eugene Glova <eugene.glova@gmail.com>
2017-03-04 23:20:33 +0300

    Add original dimensions from svg files into file object

--- server/lib/files.js
+++ server/lib/files.js
@@ -1,6 +1,16 @@
 import path from 'path';
 import fs from 'fs';
 
+const getViewBoxDimensions = dir => file => {
+  const fileContent = fs.readFileSync(path.join(dir, file), 'utf-8');
+  const matches = fileContent.match(/viewBox=\"0 0 (\d+(?:\.\d+)?) (\d+(?:\.\d+)?)\"/);
+  if ( ! matches) return;
+  return {
+    width: parseFloat(matches[1]),
+    heigth: parseFloat(matches[2])
+  }
+};
+
 const walkSync = function(dir) {
   const isDirectory = dir => file => fs.statSync(path.join(dir, file)).isDirectory();
   const filter = dir => file => file.match(/\.svg$/) || isDirectory(dir)(file);
@@ -14,7 +24,8 @@ const walkSync = function(dir) {
     } else {
       return {
         isFile: true,
-        id: file
+        id: file,
+        style: getViewBoxDimensions(dir)(file)
       };
     }
   });
