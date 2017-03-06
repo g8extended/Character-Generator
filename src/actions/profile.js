80f1fc0becbabbecf6ca45e9f112e49fb43dce03
Eugene Glova <eugene.glova@gmail.com>
2017-03-06 15:09:28 +0300

    Use arrows to move current asset on profile

--- src/actions/profile.js
+++ src/actions/profile.js
@@ -2,7 +2,8 @@ import {
   UPDATE_PROFILE_ASSET,
   UPDATE_PROFILE_ASSET_TYPE,
   UPDATE_PROFILE_ASSET_COLOR,
-  UPDATE_PROFILE_ASSET_VISIBILITY
+  UPDATE_PROFILE_ASSET_VISIBILITY,
+  MOVE_PROFILE_ASSET_TYPE
 } from '../constants/profile';
 import { getIndexByOffset, getFiles } from '../utils/files';
 
@@ -51,5 +52,23 @@ export const toggleProfileAssetVisible = () => (dispatch, getState) => {
     payload: {
       visible: ! profile[assets.current.asset].visible
     }
+  });
+};
+
+export const moveProfileAssetType = which => (dispatch, getState) => {
+  const { assets: { items, current: { asset } }, profile } = getState();
+  const { type, color } = profile[asset];
+  const { left, top } = items[asset].types[type].colors[color].files[0].style;
+  const payload = {
+    left: which % 2 ? left - (2 - which % 4) : left,
+    top: which % 2 ? top : top + (1 - which % 4)
+  };
+  dispatch({
+    type: MOVE_PROFILE_ASSET_TYPE,
+    current: {
+      asset,
+      type,
+    },
+    payload
   });
 };
--- src/components/slider/profile.js
+++ src/components/slider/profile.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router';
 import { assetClick } from '../../actions/assets';
 import map from 'lodash/map';
 import { getIndexByOffset, getFiles } from '../../utils/files';
+import { moveProfileAssetType } from '../../actions/profile';
 
 const getFile = (assetItem, profile, assets) => {
   const files = getFiles({
@@ -31,25 +32,42 @@ const getImage = (assetItem, profile, assets) => {
   };
 };
 
-const Profile = ({ dispatch, profile, assets }) => {
-  if ( ! assets.items) return <div />;
+class Profile extends Component {
+  constructor() {
+    super();
+  }
 
-  const items = map(assets.items).filter(assetItem => profile[assetItem.id].visible);
-  items.sort((a, b) => a.sortOrder - b.sortOrder);
+  componentDidMount() {
+    const { dispatch } = this.props;
+    document.addEventListener('keydown', e => {
+      if ( ! [37, 38, 39, 40].includes(e.which)) return;
+      e.preventDefault();
+      dispatch(moveProfileAssetType(e.which));
+    });
+  }
 
-  return (
-    <div className="profile"> 
-      <div className="character">
-        {map(items, assetItem => assetItem.clickable ? (
-          <Link key={assetItem.id} to={`/assets/${assetItem.id}`}>
-            <img {...getImage(assetItem, profile, assets)} />
-          </Link>
-        ) : (
-          <img key={assetItem.id} {...getImage(assetItem, profile, assets)} />
-        ))}
+  render() {
+    const { dispatch, profile, assets } = this.props;
+
+    if ( ! assets.items) return <div />;
+
+    const items = map(assets.items).filter(assetItem => profile[assetItem.id].visible);
+    items.sort((a, b) => a.sortOrder - b.sortOrder);
+
+    return (
+      <div className="profile"> 
+        <div className="character">
+          {map(items, assetItem => assetItem.clickable ? (
+            <Link key={assetItem.id} to={`/assets/${assetItem.id}`}>
+              <img {...getImage(assetItem, profile, assets)} />
+            </Link>
+          ) : (
+            <img key={assetItem.id} {...getImage(assetItem, profile, assets)} />
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default connect(({ profile, assets }) => ({ profile, assets }))(Profile);
