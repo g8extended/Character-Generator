39f4619098ac3b4a2dd5b025bdde2efa2a1fa2a7
Eugene Glova <eugene.glova@gmail.com>
2017-03-08 23:30:10 +0300

    Use urls for arrows

--- src/components/slider/arrow.js
+++ src/components/slider/arrow.js
@@ -3,19 +3,25 @@ import { connect } from 'react-redux';
 import classNames from 'classnames';
 import { updateProfileAssetFileIndex } from '../../actions/profile';
 import { isConflicts } from '../../utils/conflicts';
+import { Link } from 'react-router';
+import { getIndexByOffset, getFiles } from '../../utils/files';
 
 const Arrow = (
   ({ dispatch, assets, profile, type }) => {
-      const url = `/i/arrow_${type}.svg`;
-      const offset = type !== 'left' ? 1 : -1;
+    const url = `/i/arrow_${type}.svg`;
+    const offset = type !== 'left' ? 1 : -1;
 
-      const conflict = isConflicts(assets, profile);
+    const conflict = isConflicts(assets, profile);
 
-      return (
-        <div className={classNames('arrow', { disabled: conflict })} onClick={() => conflict || dispatch(updateProfileAssetFileIndex(offset))}>
-          <img src={url} />
-        </div>
-      );  
+    const files = getFiles(assets);
+    const fileIndex = profile[assets.current.asset].fileIndex;
+    const file = files[getIndexByOffset(files.length, fileIndex, offset)];
+
+    return (
+      <Link to={`/assets/${assets.current.asset}/${file.type}/${file.color}`} className={classNames('arrow', { disabled: conflict })} onClick={() => conflict || dispatch(updateProfileAssetFileIndex(offset))}>
+        <img src={url} />
+      </Link>
+    );  
   }
 );
 
