e9d37ec2e8957bbbb32bd0ff97849cd39b987da9
Eugene Glova <eugene.glova@gmail.com>
2017-03-09 14:40:14 +0300

    Crop svg result file by its content

--- server/lib/profile.js
+++ server/lib/profile.js
@@ -18,10 +18,26 @@ export const generateSVG = (profileEncoded, payload) => {
   const items = map(assets.items);
   items.sort((a, b) => a.sortOrder - b.sortOrder);
   const files = map(items, assetItem => getFile(assetItem, profile, assets)).filter(Boolean);
-  const svgs = files.map((file, index) => fs.readFileSync(path.join('public', file.src), 'utf-8').replace('<?xml version="1.0" encoding="utf-8"?>', '').replace(/viewBox="[^"]+"\s/, '').replace(/(st\d+)/g, 'file' + index + '\$1').replace('x="0px"', `x="${file.style.left}"`).replace('y="0px"', `y="${file.style.top}"`));
+  const resized = files.reduce((memo, file) => ({
+    width: Math.max(memo.width || 0, file.style.width + file.style.left),
+    height: Math.max(memo.height || 0, file.style.height + file.style.top),
+    left: Math.min(memo.left || Infinity, file.style.left),
+    top: Math.min(memo.top || Infinity, file.style.top)
+  }), {});
+  const resizedFiles = files.map(file => ({
+    ...file,
+    style: {
+      ...file.style,
+      left: file.style.left - resized.left,
+      top: file.style.top - resized.top
+    }
+  }));
+  const viewBoxWidth = resized.width - resized.left;
+  const viewBoxHeight = resized.height - resized.top;
+  const svgs = resizedFiles.map((file, index) => fs.readFileSync(path.join('public', file.src), 'utf-8').replace('<?xml version="1.0" encoding="utf-8"?>', '').replace(/viewBox="[^"]+"\s/, '').replace(/(st\d+)/g, 'file' + index + '\$1').replace('x="0px"', `x="${file.style.left}"`).replace('y="0px"', `y="${file.style.top}"`));
   const svgContent = [
     '<?xml version="1.0" encoding="utf-8"?>',
-    '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 900 900" xml:space="preserve">'
+    `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 ${viewBoxWidth} ${viewBoxHeight}" xml:space="preserve">`
   ].concat(svgs).concat(['</svg>']).join('\n');
   const id = md5(svgContent);
   const file = `files/${id}.svg`;
