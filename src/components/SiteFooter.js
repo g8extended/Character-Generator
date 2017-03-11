46c4432bb31583e70f55db7225445df5d5d3f7be
Eugene Glova <eugene.glova@gmail.com>
2017-03-11 16:56:17 +0300

    Fix footer icons

--- src/components/SiteFooter.js
+++ src/components/SiteFooter.js
@@ -1,31 +1,65 @@
 import React from 'react';
+import SvgIcons from './SvgIcons';
 
 export default (
   () => (
     <div className="site-footer">
-      <div className="links">
-        <a href="http://soryan.me" target="_blank">
-          Developed by <span>G8.extented</span>
-        </a>
-        <a href="https://twitter.com/KavoonMe" target="_blank">
-          <i className="twitter"></i>Twitter
-        </a>
+      <SvgIcons />
+      <ul className="links">
+        <li>
+          <a href="http://soryan.me" target="_blank">
+            Developed by <span>G8.extented</span>
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/KavoonMe" target="_blank">
+            <svg className="icon twitter">
+              <use xlinkHref="#twitter-icon"></use>
+            </svg>
+            Twitter
+          </a>
+        </li>
+        <li>
         <a href="https://www.facebook.com/kavoonme/?fref=ts" target="_blank">
-          <i className="facebook"></i>Facebook
+          <svg className="icon facebook">
+            <use xlinkHref="#facebook-icon"></use>
+          </svg>
+          Facebook
         </a>
-        <a href="https://creativemarket.com/kavoon" target="_blank">
-          <i className="creativemarket"></i>CreativeMarket
-        </a>
-        <a href="https://www.behance.net/KavoonMe" target="_blank">
-          <i className="behance"></i>Behance
-        </a>
-        <a href="https://dribbble.com/Kavoon" target="_blank">
-          <i className="dribbble"></i>Dribbble
-        </a>
-        <a href="mailto:kavoonme@gmail.com" target="_blank">
-          <i className="kavoon"></i>kavoon@gmail.com
-        </a>
-      </div>
+        </li>
+        <li>
+          <a href="https://creativemarket.com/kavoon" target="_blank">
+            <svg className="icon creativemarket">
+              <use xlinkHref="#creativemarket-icon"></use>
+            </svg>
+            CreativeMarket
+          </a>
+        </li>
+        <li>
+          <a href="https://www.behance.net/KavoonMe" target="_blank">
+            <svg className="icon behance">
+              <use xlinkHref="#behance-icon"></use>
+            </svg>
+            Behance
+          </a>
+        </li>
+        <li>
+          <a href="https://dribbble.com/Kavoon" target="_blank">
+            <svg className="icon dribbble">
+              <use xlinkHref="#dribbble-icon"></use>
+            </svg>
+            Dribbble
+          </a>
+        </li>
+        <li>
+          <a href="mailto:kavoonme@gmail.com" target="_blank">
+            <svg className="icon mail">
+              <use xlinkHref="#mail-icon"></use>
+            </svg>
+            kavoon@gmail.com
+          </a>
+        </li>
+      </ul>
     </div>
   )
 );
