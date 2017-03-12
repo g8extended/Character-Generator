f54eb65c14b4c565c5ba0b96d7bb447285f45d8c
Alexei Online <alexei.online.work@gmail.com>
2017-03-12 12:40:56 +0300

    some code updates

--- src/components/slider/ArrowImage.js
+++ src/components/slider/ArrowImage.js
@@ -3,12 +3,12 @@ import React from 'react';
 const ArrowImage = (
   ({ type, file }) => {
     const url = `/i/arrow_${type}.svg`;
+    const { top, height } = file.computedStyle;
+    const style = {
+    	top: parseFloat(top) + parseFloat(height) / 2 + '%'
+    };
 
-    return (
-      <img src={url} style={{
-        top: parseFloat(file.computedStyle.top) + parseFloat(file.computedStyle.height) / 2 + '%'
-      }} />
-    );  
+    return <img src={url} style={style} />;
   }
 );
 
