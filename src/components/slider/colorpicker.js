8dec20066d10ec9549eb1ecbdeccfa98a7842891
Alexei Online <alexei.online.work@gmail.com>
2017-03-13 17:53:10 +0300

    update colorpicker

--- src/components/slider/colorpicker.js
+++ src/components/slider/colorpicker.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import map from 'lodash/map';
+import { map, find } from 'lodash';
 import classNames from 'classnames';
 
 function getStyles(color, isActive, isDoubleColor) {
   return  {
-    background: isDoubleColor ? `linear-gradient(to right, ${color[0]} 0%, ${color[0]} 50%, ${color2} 50%, ${color[1]} 100%)` : undefined,
-    backgroundColor: color[1],
+    background: isDoubleColor ? `linear-gradient(to right, ${color[0]} 0%, ${color[0]} 50%, ${color[1]} 50%, ${color[1]} 100%)` : undefined,
+    backgroundColor: isDoubleColor || color[1],
     borderColor: isActive ? typeof color[2] === 'string' ? color[2] : color[color[2]] : undefined
   };
 }
+
+const doubleColors = [
+  {id: 'black_pink', colors: ['#36516e', '#f93b58', 0]},
+  {id: 'pink_black', colors: ['#f93b58', '#36516e', 1]},
+  {id: 'white_blue', colors: ['#45c0e9', '#efefef', 0]},
+  {id: 'blue_white', colors: ['#45c0e9', '#efefef', 1]},
+  {id: 'beige_black', colors: ['#eadaca', '#433947', 1]},
+  {id: 'black_white', colors: ['#433947', '#efefef', 0]}
+];
 
 const ColorPicker = ({ colors, current: { asset, type, color }, urlPrefix, onClick }) => {
   if (Object.keys(colors).length < 2) return <div />;
@@ -27,44 +36,21 @@ const ColorPicker = ({ colors, current: { asset, type, color }, urlPrefix, onCli
         const isDoubleColor = colorItem.id.indexOf('_') !== -1;
 
         const file = colorItem.files[0];
-        let color1 = isEyes ? isSpecialType ? file.svgColors[2] : file.svgColors[2] : file.svgColors[0];
-        let color2 = isEyes ? isSpecialType ? file.svgColors[3] : file.svgColors[5] : file.svgColors[1];
-        let borderColor = file.svgColors[file.svgColors.length - 1];
+        const color1 = isEyes ? isSpecialType ? file.svgColors[2] : file.svgColors[2] : file.svgColors[0];
+        const color2 = isEyes ? isSpecialType ? file.svgColors[3] : file.svgColors[5] : file.svgColors[1];
+        const borderColor = file.svgColors[file.svgColors.length - 1];
 
-        let color = [color1, color2, borderColor];
-
-        if (colorItem.id === 'black_pink') {
-          color = ['#36516e', '#f93b58', 0];
-        }
-
-        if (colorItem.id === 'pink_black') {
-          color = ['#f93b58', '#36516e', 1];
-        }
-
-        if (colorItem.id === 'white_blue') {
-          color = ['#45c0e9', '#efefef', 0];
-        }
-
-        if (colorItem.id === 'blue_white') {
-          color = ['#45c0e9', '#efefef', 1];
-        }
-
-        if (colorItem.id === 'beige_black') {
-          color = ['#eadaca', '#433947', 1];
-        }
-
-        if (colorItem.id === 'black_white') {
-          color = ['#433947', '#efefef', 0];
-        }
+        const finded = find(doubleColors, {id: colorItem.id});
+        const colors = finded ? finded.colors : [color1, color2, borderColor];
+        const style  = getStyles(colors, isActive, isDoubleColor);
 
         return (
           <Link key={colorItem.id}
             to={`${urlPrefix}${colorItem.id}`}
-            style={getStyles(color, isActive, isDoubleColor)}
+            style={style}
             className={className} activeClassName="active"
             onClick={() => onClick(colorItem.id)}
-          >
-          </Link>
+          />
         );
       })}
     </div>
