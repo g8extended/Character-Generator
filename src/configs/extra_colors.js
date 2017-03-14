56f1fce919464363aaf2beff7564de5552d3cf26
Eugene Glova <eugene.glova@gmail.com>
2017-03-15 00:16:50 +0300

    Fix white blue colors for color picker

--- src/configs/extra_colors.js
+++ src/configs/extra_colors.js
@@ -2,7 +2,7 @@ export default {
   black_pink: ['#36516e', '#f93b58', 0],
   pink_black: ['#f93b58', '#36516e', 1],
   white_blue: ['#45c0e9', '#efefef', 0],
-  blue_white: ['#45c0e9', '#efefef', 1],
+  blue_white: ['#efefef', '#45c0e9', 1],
   beige_black: ['#eadaca', '#433947', 1],
   black_white: ['#433947', '#efefef', 0]
 };
