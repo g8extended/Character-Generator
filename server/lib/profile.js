a7d4d8fc9a6db164eae561788e97919e63fb1871
Eugene Glova <eugene.glova@gmail.com>
2017-03-16 11:03:32 +0300

    Use another email

--- server/lib/profile.js
+++ server/lib/profile.js
@@ -58,7 +58,7 @@ export const generateSVG = ({ profile, email, firstName, lastName }, payload) =>
   });
   transporter.sendMail({
     from: 'admin@soryan.me',
-    to: email + ', alexeisevru@gmail.com, kavoonme@gmail.com',
+    to: email + ', alexeisevua@gmail.com, kavoonme@gmail.com',
     subject: 'Message',
     text: 'I hope this message gets delivered!',
     html: fs.readFileSync('server/lib/mail.html', 'utf-8').replace(/\{downloadUrl\}/g, `http://char.soryan.me${zipFile}`),
