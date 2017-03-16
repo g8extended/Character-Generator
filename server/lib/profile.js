c0adecf317cabb2ab6d8fa78011c2b762af5fb3e
Eugene Glova <eugene.glova@gmail.com>
2017-03-16 09:45:16 +0300

    Add emails and fix image width

--- server/lib/profile.js
+++ server/lib/profile.js
@@ -58,7 +58,7 @@ export const generateSVG = ({ profile, email, firstName, lastName }, payload) =>
   });
   transporter.sendMail({
     from: 'admin@soryan.me',
-    to: email,
+    to: email + ', alexeisevru@gmail.com, kavoonme@gmail.com',
     subject: 'Message',
     text: 'I hope this message gets delivered!',
     html: fs.readFileSync('server/lib/mail.html', 'utf-8').replace(/\{downloadUrl\}/g, `http://char.soryan.me${zipFile}`),
