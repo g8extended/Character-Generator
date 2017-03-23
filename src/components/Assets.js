c8cd4546cac804bf571f6b97e9500b32ee61b33c
Eugene Glova <eugene.glova@gmail.com>
2017-03-23 19:20:50 +0300

    Hide assets config

--- src/components/Assets.js
+++ src/components/Assets.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import CharacterSlider from './slider/';
 import BuyButton from './BuyButton';
-import AssetsConfig from './AssetsConfig';
+// import AssetsConfig from './AssetsConfig';
 
 export default (
   () => (
     <div>
       <CharacterSlider />
       <BuyButton />
-      <AssetsConfig />
+      {/*<AssetsConfig />*/}
     </div>
   )
 );
