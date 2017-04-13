5ecff6a36c80be5f87426f86da0e46cd454231ae
Eugene Glova <eugene.glova@gmail.com>
2017-04-13 22:09:32 +0300

    Make Success to be thankyou page and check for payment on load

--- src/components/Success.js
+++ src/components/Success.js
@@ -1,37 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
-import HeaderNav from './HeaderNav';
-import { buyAll } from '../actions/sale';
+import Profile from './slider/profile';
+import { checkPayment } from '../actions/checkout';
 
-export default connect()(
-  ({ dispatch }) => (
-  <div className="success-page">
-    <div className="success-icon">
-      <img src="http://placehold.it/33x33" alt="success icon" />
-    </div>
-    <h2 className="blue-header">PAYMENT COMPLETE</h2>
-    <div className="text">
-      You can download your character here or in email
-    </div>
-    <div className="character-slider">
-      <div className="profile">
-        <div className="character-wrapper">
-          <div className="character">
-            <img src="/svg/Body/01/default/body.svg" style={{
-              width:'61%',
-              height:'61%',
-              left:'20%',
-              top:'18%'
-            }} alt="Kavoon character" />
-          </div>
+class Success extends Component {
+  constructor() {
+    super();
+  }
+
+  componentDidMount() {
+    const { dispatch, query } = this.props;
+    dispatch(checkPayment(query.sale_id));
+  }
+
+  render() {
+    const { downloadUrl } = this.props;
+    return (
+      <div className="success-page">
+        <div className="success-icon">
+          <img src="http://placehold.it/33x33" alt="success icon" />
+        </div>
+        <h2 className="blue-header">PAYMENT COMPLETE</h2>
+        <div className="text">
+          You can download your character here or in email
+        </div>
+        <div className="character-slider">
+          <Profile />
+        </div> 
+        <div>
+          {downloadUrl && <a href={`${downloadUrl}`} className="button">Download character</a>}
+          <Link to="/" className="link">GO TO HOME PAGE</Link>
         </div>
       </div>
-    </div> 
-    <div>
-      <a href="#" className="button">Download character</a>
-      <Link to="/" className="link">GO TO HOME PAGE</Link>
-    </div>
-  </div>
-  )
-);
+    );
+  }
+}
+
+export default connect(({ routing, checkout: { downloadUrl } }) => ({
+  query: routing.locationBeforeTransitions.query,
+  downloadUrl
+}))(Success);
