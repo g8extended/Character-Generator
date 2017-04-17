74aa572470d81a1b58218f1556574d66766c9e7f
Eugene Glova <eugene.glova@gmail.com>
2017-04-17 15:48:40 +0300

    Add google analytics

--- src/components/Router.js
+++ src/components/Router.js
@@ -5,10 +5,22 @@ import App from './App';
 import Success from './Success';
 import Assets from './Assets';
 import { setRouter } from '../actions/assets';
+import ReactGA from 'react-ga';
+
+if (typeof window !== 'undefined') {
+  ReactGA.initialize('UA-97466907-1');
+}
+
+function logPageView() {
+  if (typeof window !== 'undefined') {
+    ReactGA.set({ page: window.location.pathname });
+    ReactGA.pageview(window.location.pathname);
+  }
+}
 
 export default connect()(
   ({ dispatch, history }) => (
-    <Router history={history}>
+    <Router history={history} onUpdate={logPageView}>
       <Route path="/" component={App}>
         <IndexRedirect to="/assets" />
         <Route path="thank_you" component={Success} />
