b6c7ecc078c03cff940255f226c64f4ff537874b
Eugene Glova <eugene.glova@gmail.com>
2017-04-23 20:45:53 +0300

    Fix filters

--- src/utils/filters.js
+++ src/utils/filters.js
@@ -2,8 +2,7 @@ import filter from 'lodash/filter';
 
 export const filterColors = (profile, asset, colors) => {
   return filter(colors, color => {
-    if (asset !== 'Mouths') return true;
-    if (asset !== 'Eyes') return true;
+    if ( ! ['Mouths', 'Eyes'].includes(asset)) return true;
     return color.id.indexOf(profile.Body.color) !== -1;
   });
 };
